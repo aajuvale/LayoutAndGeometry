4175c8abbd3ed50d32a8c44c8a473cedf5024fb2
aajuvale <ahmed.juvale@gmail.com>
2025-08-19 20:29:28 -0700

    feat: implemented resized images w/ geometry reader

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .offset(x: 100, y: 100)
-            .background(.red)
+//        GeometryReader { proxy in
+//            Image(.example)
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: proxy.size.width * 0.8, height: 300)
+//        }
+
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+//            Image(.example)
+//                .resizable()
+//                .scaledToFit()
+//                .containerRelativeFrame(.horizontal) { size, axis in
+//                    size * 0.8
+//                }
+            GeometryReader { proxy in
+                Image(.example)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
+        }
     }
 }
 
